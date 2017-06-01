abe35c79574f301336211776d92be14967dc0855
hantusa <hantusa@hotmail.com>
2017-06-01 15:20:06 -0400

    Create save.php

---
+++ save.php
@@ -0,0 +1,14 @@
+<?php
+
+include "./db_connect.php";
+
+$phoneno = $_GET['phoneno'];
+$pushedno = $_GET['pushedno'];
+	
+$query = "insert condotext_results(cr_phoneno,cr_condo_no,cr_enable) into values('".$phoneno."','".$pushedno."','Y')" ;
+
+		//echo $query;
+
+$connect->query($query);
+
+?>
