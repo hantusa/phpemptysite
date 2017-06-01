258294f250cbfde0f5c68534d8d4d92271d34c59
hantusa <hantusa@hotmail.com>
2017-06-01 15:45:33 -0400

    Update save.php

--- save.php
+++ save.php
@@ -4,10 +4,27 @@ include "./db_connect.php";
 
 $phoneno = $_GET['phoneno'];
 $pushedno = $_GET['pushedno'];
+$from =$_GET['From'];
+
+$user_pushed = $_REQUEST['Digits'];
+			
+	        if ( $user_pushed == '1' )
+	        {	
+	          	
+	        		//echo "<Sms to='16472060140'>Text from 1</Sms>";
+	        		echo "<Say voice='woman' language='ko-KR'>일번을 선택했습니다.</Say>";
+	        		//$url = "save.php?phoneno=".$_REQUEST['From']."&pushedno=".$user_pushed;
+			        echo "<Sms to=".$from.">Text from 1</Sms>";
+ 
+	        	
+	        }
+
+
+
 	
 $query = "insert into condotext_results(cr_phoneno,cr_condo_no,cr_enable)  values('".$phoneno."','".$pushedno."','Y')" ;
 
-		echo $query;
+		//echo $query;
 
 $connect->query($query);
 ?>
