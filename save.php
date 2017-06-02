8525c4cbe84d42c3b0eda45125565c258b2d307e
hantusa <hantusa@hotmail.com>
2017-06-02 11:21:11 -0400

    Update save.php

--- save.php
+++ save.php
@@ -17,36 +17,46 @@ $k_user_pushed = $user_pushed;
 switch ($k_user_pushed) {
     case "1":
         echo "일번";
+	$query = "select ct_comment_01 from condotext where ct_enable = 'Y' and ct_comment_01_no = 1 " ;
         break;
     case "2":
         echo "이번";
+	$query = "select ct_comment_02 from condotext where ct_enable = 'Y' and ct_comment_02_no = 2 " ;
         break;
     case "3":
         echo "3번";
+	$query = "select ct_comment_03 from condotext where ct_enable = 'Y' and ct_comment_03_no = 3 " ;
         break;
 		
     case "4":
         echo "사번";
+	$query = "select ct_comment_04 from condotext where ct_enable = 'Y' and ct_comment_04_no = 4 " ;
         break;
     case "5":
         echo "오번";
+	$query = "select ct_comment_05 from condotext where ct_enable = 'Y' and ct_comment_05_no = 5 " ;
         break;
     case "6":
         echo "육번";
+	$query = "select ct_comment_06 from condotext where ct_enable = 'Y' and ct_comment_06_no = 6 " ;
         break;
 		
     case "7":
         echo "칠번";
+	$query = "select ct_comment_07 from condotext where ct_enable = 'Y' and ct_comment_07_no = 7 " ;
         break;
     case "8":
         echo "팔번";
+	$query = "select ct_comment_08 from condotext where ct_enable = 'Y' and ct_comment_08_no = 8 " ;
         break;
     case "9":
         echo "구번";
+	$query = "select ct_comment_09 from condotext where ct_enable = 'Y' and ct_comment_09_no = 9 " ;
         break;	
 		
     case "10":
-        echo "번";
+        echo "십번";
+	$query = "select ct_comment_10 from condotext where ct_enable = 'Y' and ct_comment_10_no = 10 " ;
         break;			
     default:
         echo "모두";
@@ -61,8 +71,16 @@ switch ($k_user_pushed) {
 				echo "<Say voice='woman' language='ko-KR'>You can cancel later</Say>";
 	        		//$url = "save.php?phoneno=".$_REQUEST['From']."&pushedno=".$user_pushed;
 
+$result = $connect->query($query);
 
-			        echo "<Sms to='".$phoneno."'>Text from 1</Sms>";
+    
+while($row = $result->fetch_array())
+{
+	$ct_comment= $row[0];
+			
+}
+
+			        echo "<Sms to='".$phoneno."'>".$ct_comment."</Sms>";
  
 	        	
 	      //  }
